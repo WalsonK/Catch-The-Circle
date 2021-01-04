07b1db7c5cd36cc4b2cc275edbe825afecaba2a1
WalsonK <71435596+WalsonK@users.noreply.github.com>
2021-01-04 17:40:08 +0100

    UX : login
    Login UX :+1:
    Icons of dasboard : work in progress

---
+++ catchTheCircle/Assets/Script/Menu/connexion/ChangeInput.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ChangeInput : MonoBehaviour
+{
+
+    EventSystem system;
+    public Button submitBtn;
+
+    //To avoid error of no first input selected :
+    public Selectable firstInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = EventSystem.current;
+        firstInput.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Shit to go Up
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Selectable previous = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+            if (previous != null)
+            {
+                previous.Select();
+            }
+        }
+        //Tab to go down 
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+            if (next != null)
+            {
+                next.Select();
+            }
+        }
+        //Enter to submit form !
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            submitBtn.onClick.Invoke();
+            Debug.Log("Btn pressed !");
+        }
+    }
+}
