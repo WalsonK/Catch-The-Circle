f72be9772984706033aa2514d6d5afa578fcec17
Driss Kanouni <driss.kanouni@outlook.fr>
2021-02-01 16:56:35 +0100

    Added: Register Script
    En cours, mais pas fini

---
+++ catchTheCircle/Assets/Script/Database/Register.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour
+{
+
+    public InputField namefield;
+    public InputField passwordField;
+    public Button submitButton;
+
+
+}
